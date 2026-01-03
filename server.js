af2cc06e16b37222fa048abc5d451a2803356cec
shadexqz <arikraspopov3@gmail.com>
2026-01-03 19:02:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
@@ -9,26 +8,29 @@ const io = new Server(server, { cors: { origin: "*" } });
 let players = {};
 
 io.on('connection', (socket) => {
-    console.log('Игрок подключился:', socket.id);
-    players[socket.id] = { x: 300, y: 300, color: `hsl(${Math.random() * 360}, 70%, 60%)` };
+    // Генерируем цвет один раз при подключении
+    const hue = Math.floor(Math.random() * 360);
+    players[socket.id] = { 
+        x: 300, y: 300, 
+        color: `hsl(${hue}, 80%, 60%)`,
+        angle: 0,
+        hook: { active: false, x: 0, y: 0 }
+    };
 
-    socket.emit('currentPlayers', players);
+    socket.emit('init', { id: socket.id, players });
     socket.broadcast.emit('newPlayer', { id: socket.id, player: players[socket.id] });
 
-    socket.on('playerMovement', (data) => {
+    socket.on('update', (data) => {
         if (players[socket.id]) {
-            players[socket.id].x = data.x;
-            players[socket.id].y = data.y;
-            players[socket.id].angle = data.angle;
-            players[socket.id].hook = data.hook;
-            socket.broadcast.emit('playerMoved', { id: socket.id, ...players[socket.id] });
+            Object.assign(players[socket.id], data);
+            socket.broadcast.emit('playerUpdated', { id: socket.id, ...data });
         }
     });
 
     socket.on('disconnect', () => {
         delete players[socket.id];
-        io.emit('playerDisconnected', socket.id);
+        io.emit('playerLeave', socket.id);
     });
 });
 
-server.listen(process.env.PORT || 3000, () => console.log('Сервер запущен!'));
+server.listen(process.env.PORT || 3000);
